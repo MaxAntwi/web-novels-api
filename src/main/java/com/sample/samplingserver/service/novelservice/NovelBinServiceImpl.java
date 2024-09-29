a19a39a81ccc388ae62e4385660cf70524386d56
Maxwell Antwi <theonlymaxmc@gmail.com>
2024-09-29 08:53:01 -0700

    fix: simulate browser request

--- src/main/java/com/sample/samplingserver/service/novelservice/NovelBinServiceImpl.java
+++ src/main/java/com/sample/samplingserver/service/novelservice/NovelBinServiceImpl.java
@@ -28,7 +28,8 @@ public class NovelBinServiceImpl implements NovelBinService {
         List<NovelDto> hotNovels = new ArrayList<>();
         log.info("Getting Hot Novels");
         try {
-            Document doc = Jsoup.connect(url).get();
+            Document doc = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3").get();
             Elements novels = doc.select(".index-novel .item");
             for (Element novel : novels) {
                 String title = novel.select("h3").text();
